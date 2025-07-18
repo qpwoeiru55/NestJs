4c1d0b461980ce7cf9cade111a310e032acf6cf1
DESKTOP-1EDMQ3M\82104 <wlstjrwlstjr18@gmail.com>
2025-07-18 21:30:13 +0900

    app controller/service

--- neflix/src/app.controller.ts
+++ neflix/src/app.controller.ts
@@ -1,64 +1,41 @@
-import { Controller, Delete, Get, NotFoundException, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { error } from 'console';
-
-interface Movie {
-  id: number;
-  title: string;
-}
 
 @Controller('movie')
 export class AppController {
 
-  private movies: Movie[] = [
-    {
-      id: 1,
-      title: '해리',
-    },
-    {
-      id: 2,
-      title: '반지',
-    }
-  ];
-
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  getMovies() {
-    return this.movies;
+  getMovies(
+    @Query('title') title?: string,
+  ) {
+    return this.appService.getManyMovies(title);
   }
 
   @Get(':id')
   getMovie(@Param('id') id: string) {
-    const movie = this.movies.find((m) => m.id === +id);
-
-    if(!movie){
-      throw new NotFoundException('존재하지 않는 id의 영화');
-    }
-
-    return movie;
+    return this.appService.getMovieById(+id);
   }
 
   @Post()
-  postMovie() {
-    return {
-      id: 3,
-      name: '어벤져스',
-      character: ['아연맨', '캡틴'],
-    }
+  postMovie(
+    @Body('title') title: string,
+  ) {
+    return this.appService.createMovie(title);
   }
 
   @Patch(':id')
-  patchMovie() {
-    return {
-      id: 3,
-      name: '어벤져스',
-      character: ['아연맨', '블랙'],
-    };
+  patchMovie(
+    @Param('id') id: string,
+    @Body('title') title: string,
+  ) {
+    return this.appService.updateMovie(+id, title);
   }
 
   @Delete(':id')
-  deleteMoive() {
-    return 3;
+  deleteMoive(@Param('id') id: string) {
+    return this.appService.deleteMovie(+id);
   }
 }
