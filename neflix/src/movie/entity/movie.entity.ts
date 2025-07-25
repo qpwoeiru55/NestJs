4f631e681ac140ed8383d2bc5015b40ad5f788bb
DESKTOP-1EDMQ3M\82104 <wlstjrwlstjr18@gmail.com>
2025-07-25 20:41:12 +0900

    orm relationship

--- neflix/src/movie/entity/movie.entity.ts
+++ neflix/src/movie/entity/movie.entity.ts
@@ -1,7 +1,8 @@
 import { Exclude } from 'class-transformer';
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { BaseTable } from './base-table.entity';
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseTable } from '../../common/entity/base-table.entity';
 import { MovieDetail } from './movie-detail.entity';
+import { Director } from 'src/director/entitity/director.entity';
 
 @Entity()
 export class Movie extends BaseTable {
@@ -17,4 +18,7 @@ export class Movie extends BaseTable {
   @OneToOne(() => MovieDetail, (movieDetail) => movieDetail.movie, { cascade: true })
   @JoinColumn()
   detail: MovieDetail;
+
+  @ManyToOne(() => Director, (director) => director.movies)
+  director: Director;
 }
